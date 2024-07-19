e948a230bce1b92616ba99cc61fb497a21e9255d
sudonite <sudonite@gmail.com>
2024-07-19 07:58:58 +0200

    Add: Kafka Producer

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,23 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 	"github.com/sudonite/TollCalculator/types"
 )
 
+var kafkaTopic = "obudata"
+
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
+	prod  *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+
 	return &DataReceiver{
 		msgch: make(chan types.OBUData, 128),
+		prod:  p,
+	}, nil
+}
+
+func (dr *DataReceiver) produceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
 	}
+	err = dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &kafkaTopic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: b,
+	}, nil)
+	return err
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
@@ -42,13 +81,17 @@ func (dr *DataReceiver) wsReceiveLoop() {
 			fmt.Println("Read error:", err)
 			continue
 		}
-		fmt.Printf("Received OBU Data: [%d] :: <lat %.2f, long %.2f>\n", data.OBUID, data.Lat, data.Long)
-		dr.msgch <- data
+		if err := dr.produceData(data); err != nil {
+			fmt.Println("Kafka Produce error:", err)
+		}
 	}
 }
 
 func main() {
-	recv := NewDataReceiver()
+	recv, err := NewDataReceiver()
+	if err != nil {
+		log.Fatal(err)
+	}
 	http.HandleFunc("/ws", recv.handleWS)
 	http.ListenAndServe(":30000", nil)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sudonite/TollCalculator
 
 go 1.22.5
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.5.0 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
