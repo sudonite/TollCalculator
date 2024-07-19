ec3794e7dd7175862939b0003c17de81d510987f
sudonite <sudonite@gmail.com>
2024-07-19 09:47:42 +0200

    Add: Logging Middleware & Refactor

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,43 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
+	"github.com/sirupsen/logrus"
 	"github.com/sudonite/TollCalculator/types"
 )
-
-var kafkaTopic = "obudata"
 
 type DataReceiver struct {
 	msgch chan types.OBUData
 	conn  *websocket.Conn
-	prod  *kafka.Producer
+	prod  DataProducer
 }
 
 func NewDataReceiver() (*DataReceiver, error) {
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	var (
+		p          DataProducer
+		err        error
+		kafkaTopic = "obudata"
+	)
+	p, err = NewKafkaProducer(kafkaTopic)
 	if err != nil {
 		return nil, err
 	}
-
-	go func() {
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
-				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
-				}
-			}
-		}
-	}()
-
+	p = NewLogMiddleware(p)
 	return &DataReceiver{
 		msgch: make(chan types.OBUData, 128),
 		prod:  p,
@@ -45,18 +35,15 @@ func NewDataReceiver() (*DataReceiver, error) {
 }
 
 func (dr *DataReceiver) produceData(data types.OBUData) error {
-	b, err := json.Marshal(data)
-	if err != nil {
-		return err
-	}
-	err = dr.prod.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{
-			Topic:     &kafkaTopic,
-			Partition: kafka.PartitionAny,
-		},
-		Value: b,
-	}, nil)
-	return err
+	defer func(start time.Time) {
+		logrus.WithFields(logrus.Fields{
+			"obuID": data.OBUID,
+			"lat":   data.Lat,
+			"long":  data.Long,
+			"took":  time.Since(start),
+		}).Info("Producing to Kafka")
+	}(time.Now())
+	return dr.prod.ProduceData(data)
 }
 
 func (dr *DataReceiver) handleWS(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.22.5
 require (
 	github.com/confluentinc/confluent-kafka-go/v2 v2.5.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
