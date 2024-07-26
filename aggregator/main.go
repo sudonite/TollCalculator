17f30d3bd8d8e6b9bf589022fcf433cbe6d967a7
sudonite <sudonite@gmail.com>
2024-07-27 01:04:28 +0200

    Add: gRPC Client

--- aggregator/main.go
+++ aggregator/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"strconv"
@@ -56,7 +57,7 @@ func writeJSON(w http.ResponseWriter, status int, v any) error {
 
 func makeGRPCTransport(listenAddr string, svc Aggregator) error {
 	fmt.Println("gRPC transport running on", listenAddr)
-	ln, err := net.Listen("TCP", listenAddr)
+	ln, err := net.Listen("tcp", listenAddr)
 	if err != nil {
 		return err
 	}
@@ -67,11 +68,11 @@ func makeGRPCTransport(listenAddr string, svc Aggregator) error {
 	return server.Serve(ln)
 }
 
-func makeHTTPTransport(listenAddr string, svc Aggregator) {
+func makeHTTPTransport(listenAddr string, svc Aggregator) error {
 	fmt.Println("HTTP transport running on", listenAddr)
 	http.HandleFunc("/aggregate", handleAggregate(svc))
 	http.HandleFunc("/invoice", handleGetInvoice(svc))
-	http.ListenAndServe(listenAddr, nil)
+	return http.ListenAndServe(listenAddr, nil)
 }
 
 func main() {
@@ -84,6 +85,9 @@ func main() {
 	svc := NewInvoiceAggregator(store)
 	svc = NewLogMiddleware(svc)
 
-	go makeGRPCTransport(*grpcListenAddr, svc)
-	makeHTTPTransport(*httpListenAddr, svc)
+	go func() {
+		log.Fatal(makeGRPCTransport(*grpcListenAddr, svc))
+	}()
+
+	log.Fatal(makeHTTPTransport(*httpListenAddr, svc))
 }
